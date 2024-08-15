09b77c0276db44de0d2ef10b5a398c8ccdd60bf0
Maxim Rysevets <root@effcore.com>
2024-08-15 18:25:34 +0300

    Dynamic creation of "blockerList.json" (v.2) has beed added.

--- JS Blocker Rules/ContentBlockerRequestHandler.swift
+++ JS Blocker Rules/ContentBlockerRequestHandler.swift
@@ -4,30 +4,26 @@ import Foundation
 class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
 
     func beginRequest(with context: NSExtensionContext) {
-        let result: NSMutableArray = []
-
-        let data = [
+        let domains = [
             "js-blocker.com",
             "www.js-blocker.com",
             "sub2.sub1.js-blocker.com",
             "js-blocker.net",
             "js-blocker.org",
-            ".*\\.youtube.com"
+            "*youtube.com"
         ]
 
-        for row in data {
-            result.add([
-                "trigger": [
-                    "url-filter": row,
-                    "url-filter-is-case-sensitivity": true,
-                    "resource-type": ["script"]
-                ],
-                "unless-domain": [],
-                "action": [
-                    "type": "block"
-                ]
-            ])
-        }
+        let result = [[
+            "action": [
+                "type": "block"
+            ],
+            "trigger": [
+                "url-filter": ".*",
+                "url-filter-is-case-sensitivity": true,
+                "resource-type": ["script"],
+                "unless-domain": domains
+            ]
+        ]]
 
         let JSON = try! JSONSerialization.data(withJSONObject: result)
         let attachment = NSItemProvider(item: JSON as NSSecureCoding?, typeIdentifier: "public.json")
