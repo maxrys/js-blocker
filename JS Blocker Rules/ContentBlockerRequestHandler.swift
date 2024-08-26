2f45b8f35d7ea35c687c879a0bf6c186df712dd3
Maxim Rysevets <root@effcore.com>
2024-08-26 18:37:47 +0300

    Dynamic creation of "blockerList.json" (v.3) has beed added.

--- JS Blocker Rules/ContentBlockerRequestHandler.swift
+++ JS Blocker Rules/ContentBlockerRequestHandler.swift
@@ -4,26 +4,39 @@ import Foundation
 class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
 
     func beginRequest(with context: NSExtensionContext) {
-        let domains = [
-            "js-blocker.com",
-            "www.js-blocker.com",
-            "sub2.sub1.js-blocker.com",
-            "js-blocker.net",
-            "js-blocker.org",
-            "*youtube.com"
-        ]
+        var rules: [String] = []
 
-        let result = [[
-            "action": [
-                "type": "block"
-            ],
-            "trigger": [
-                "url-filter": ".*",
-                "url-filter-is-case-sensitivity": true,
-                "resource-type": ["script"],
-                "unless-domain": domains
-            ]
-        ]]
+        for domain in WhiteDomains.selectAll() {
+            if domain.withSubdomains == true {rules.append("*\(domain.name)")}
+            else                             {rules.append( "\(domain.name)")}
+        }
+
+        var result: Any = []
+
+        if !rules.isEmpty {
+            result = [[
+                "action": [
+                    "type": "block"
+                ],
+                "trigger": [
+                    "url-filter": ".*",
+                    "url-filter-is-case-sensitivity": true,
+                    "resource-type": ["script"],
+                    "unless-domain": rules
+                ]
+            ]]
+        } else {
+            result = [[
+                "action": [
+                    "type": "block"
+                ],
+                "trigger": [
+                    "url-filter": ".*",
+                    "url-filter-is-case-sensitivity": true,
+                    "resource-type": ["script"]
+                ]
+            ]]
+        }
 
         let JSON = try! JSONSerialization.data(withJSONObject: result)
         let attachment = NSItemProvider(item: JSON as NSSecureCoding?, typeIdentifier: "public.json")
@@ -33,3 +46,341 @@ class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
     }
 
 }
+
+/*
+
+    ################################################
+    ### RESEARCH OF THE RULES IN REAL CONDITIONS ###
+    ################################################
+
+    Rule: "js-blocker.com":
+
+       Domain "js-blocker.com":
+       - JS from js-blocker.com:           !!! ON !!!
+       - JS from www.js-blocker.com:       !!! ON !!!
+       - JS from sub1.js-blocker.com:      !!! ON !!!
+       - JS from sub2.sub1.js-blocker.com: !!! ON !!!
+       - JS from js-blocker.net:           !!! ON !!!
+       Domain "www.js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "sub1.js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "sub2.sub1.js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "js-blocker.net":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+
+    Rule: "*js-blocker.com":
+
+       Domain "js-blocker.com":
+       - JS from js-blocker.com:           !!! ON !!!
+       - JS from www.js-blocker.com:       !!! ON !!!
+       - JS from sub1.js-blocker.com:      !!! ON !!!
+       - JS from sub2.sub1.js-blocker.com: !!! ON !!!
+       - JS from js-blocker.net:           !!! ON !!!
+       Domain "www.js-blocker.com":
+       - JS from js-blocker.com:           !!! ON !!!
+       - JS from www.js-blocker.com:       !!! ON !!!
+       - JS from sub1.js-blocker.com:      !!! ON !!!
+       - JS from sub2.sub1.js-blocker.com: !!! ON !!!
+       - JS from js-blocker.net:           !!! ON !!!
+       Domain "sub1.js-blocker.com":
+       - JS from js-blocker.com:           !!! ON !!!
+       - JS from www.js-blocker.com:       !!! ON !!!
+       - JS from sub1.js-blocker.com:      !!! ON !!!
+       - JS from sub2.sub1.js-blocker.com: !!! ON !!!
+       - JS from js-blocker.net:           !!! ON !!!
+       Domain "sub2.sub1.js-blocker.com":
+       - JS from js-blocker.com:           !!! ON !!!
+       - JS from www.js-blocker.com:       !!! ON !!!
+       - JS from sub1.js-blocker.com:      !!! ON !!!
+       - JS from sub2.sub1.js-blocker.com: !!! ON !!!
+       - JS from js-blocker.net:           !!! ON !!!
+       Domain "js-blocker.net":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+
+    Rule: "www.js-blocker.com":
+
+       Domain "js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "www.js-blocker.com":
+       - JS from js-blocker.com:           !!! ON !!!
+       - JS from www.js-blocker.com:       !!! ON !!!
+       - JS from sub1.js-blocker.com:      !!! ON !!!
+       - JS from sub2.sub1.js-blocker.com: !!! ON !!!
+       - JS from js-blocker.net:           !!! ON !!!
+       Domain "sub1.js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "sub2.sub1.js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "js-blocker.net":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+
+    Rule: "*www.js-blocker.com":
+
+       Domain "js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "www.js-blocker.com":
+       - JS from js-blocker.com:           !!! ON !!!
+       - JS from www.js-blocker.com:       !!! ON !!!
+       - JS from sub1.js-blocker.com:      !!! ON !!!
+       - JS from sub2.sub1.js-blocker.com: !!! ON !!!
+       - JS from js-blocker.net:           !!! ON !!!
+       Domain "sub1.js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "sub2.sub1.js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "js-blocker.net":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+
+    Rule: "sub1.js-blocker.com":
+
+       Domain "js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "www.js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "sub1.js-blocker.com":
+       - JS from js-blocker.com:           !!! ON !!!
+       - JS from www.js-blocker.com:       !!! ON !!!
+       - JS from sub1.js-blocker.com:      !!! ON !!!
+       - JS from sub2.sub1.js-blocker.com: !!! ON !!!
+       - JS from js-blocker.net:           !!! ON !!!
+       Domain "sub2.sub1.js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "js-blocker.net":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+
+    Rule: "*sub1.js-blocker.com":
+
+       Domain "js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "www.js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "sub1.js-blocker.com":
+       - JS from js-blocker.com:           !!! ON !!!
+       - JS from www.js-blocker.com:       !!! ON !!!
+       - JS from sub1.js-blocker.com:      !!! ON !!!
+       - JS from sub2.sub1.js-blocker.com: !!! ON !!!
+       - JS from js-blocker.net:           !!! ON !!!
+       Domain "sub2.sub1.js-blocker.com":
+       - JS from js-blocker.com:           !!! ON !!!
+       - JS from www.js-blocker.com:       !!! ON !!!
+       - JS from sub1.js-blocker.com:      !!! ON !!!
+       - JS from sub2.sub1.js-blocker.com: !!! ON !!!
+       - JS from js-blocker.net:           !!! ON !!!
+       Domain "js-blocker.net":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+
+    Rule: "sub2.sub1.js-blocker.com":
+
+       Domain "js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "www.js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "sub1.js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "sub2.sub1.js-blocker.com":
+       - JS from js-blocker.com:           !!! ON !!!
+       - JS from www.js-blocker.com:       !!! ON !!!
+       - JS from sub1.js-blocker.com:      !!! ON !!!
+       - JS from sub2.sub1.js-blocker.com: !!! ON !!!
+       - JS from js-blocker.net:           !!! ON !!!
+       Domain "js-blocker.net":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+
+    Rule: "*sub2.sub1.js-blocker.com":
+
+       Domain "js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "www.js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "sub1.js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "sub2.sub1.js-blocker.com":
+       - JS from js-blocker.com:           !!! ON !!!
+       - JS from www.js-blocker.com:       !!! ON !!!
+       - JS from sub1.js-blocker.com:      !!! ON !!!
+       - JS from sub2.sub1.js-blocker.com: !!! ON !!!
+       - JS from js-blocker.net:           !!! ON !!!
+       Domain "js-blocker.net":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+
+    Rule: "js-blocker.net":
+
+       Domain "js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "www.js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "sub1.js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "sub2.sub1.js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "js-blocker.net":
+       - JS from js-blocker.com:           !!! ON !!!
+       - JS from www.js-blocker.com:       !!! ON !!!
+       - JS from sub1.js-blocker.com:      !!! ON !!!
+       - JS from sub2.sub1.js-blocker.com: !!! ON !!!
+       - JS from js-blocker.net:           !!! ON !!!
+
+    Rule: "*js-blocker.net":
+
+       Domain "js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "www.js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "sub1.js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "sub2.sub1.js-blocker.com":
+       - JS from js-blocker.com:           off
+       - JS from www.js-blocker.com:       off
+       - JS from sub1.js-blocker.com:      off
+       - JS from sub2.sub1.js-blocker.com: off
+       - JS from js-blocker.net:           off
+       Domain "js-blocker.net":
+       - JS from js-blocker.com:           !!! ON !!!
+       - JS from www.js-blocker.com:       !!! ON !!!
+       - JS from sub1.js-blocker.com:      !!! ON !!!
+       - JS from sub2.sub1.js-blocker.com: !!! ON !!!
+       - JS from js-blocker.net:           !!! ON !!!
+
+*/
