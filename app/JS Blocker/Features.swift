c3899d4ae13f112510a5c97437f44db7b00e6690
Maxim Rysevets <root@effcore.com>
2026-03-11 12:22:28 +0300

    NSOpenPanel dialog has been improved.

--- app/JS Blocker/Features.swift
+++ app/JS Blocker/Features.swift
@@ -23,51 +23,54 @@ final class Features {
             openPanel.canCreateDirectories = true
             openPanel.prompt = NSLocalizedString("Export", comment: "")
 
-            if (openPanel.runModal() == .OK), let directoryURL = openPanel.url {
-
-                /* MARK: Generate export JSON */
-
-                var jsonObject: [ExportImportItem] = []
-                for item in items {
-                    jsonObject.append(
-                        ExportImportItem(
-                            name    : item.name,
-                            isGlobal: item.isWildcard
-                        )
-                    )
-                }
-
-                let jsonEncoder = JSONEncoder()
-                jsonEncoder.outputFormatting = .prettyPrinted
-
-                let jsonData = try! jsonEncoder.encode(
-                    jsonObject
-                )
-
-                /* MARK: Generate export URL */
-
-                let formattedDate = Date().formatCustom("yyyyMMdd-HHmmss")
-                let exportFileURL = directoryURL.appendingPathComponent(
-                    "JSBlocker-\(formattedDate).json"
-                )
-
-                /* MARK: Write to file */
-
-                try jsonData.stringUTF8!.write(
-                    to: exportFileURL,
-                    atomically: false,
-                    encoding: .utf8
-                )
-
-                /* MARK: message */
-
-                MessageBox.insert(
-                    type: .ok,
-                    title: String(format: NSLocalizedString("%d records have been exported", comment: ""), jsonObject.count),
-                    lifeTime: .time(3)
-                )
-
+            guard openPanel.runModal() == .OK else {
+                return
             }
+            guard let directoryURL = openPanel.url else {
+                return
+            }
+
+            /* MARK: Generate export JSON */
+
+            var jsonObject: [ExportImportItem] = []
+            for item in items {
+                jsonObject.append(
+                    ExportImportItem(
+                        name    : item.name,
+                        isGlobal: item.isWildcard
+                    )
+                )
+            }
+
+            let jsonEncoder = JSONEncoder()
+            jsonEncoder.outputFormatting = .prettyPrinted
+
+            let jsonData = try! jsonEncoder.encode(
+                jsonObject
+            )
+
+            /* MARK: Generate export URL */
+
+            let formattedDate = Date().formatCustom("yyyyMMdd-HHmmss")
+            let exportFileURL = directoryURL.appendingPathComponent(
+                "JSBlocker-\(formattedDate).json"
+            )
+
+            /* MARK: Write to file */
+
+            try jsonData.stringUTF8!.write(
+                to: exportFileURL,
+                atomically: false,
+                encoding: .utf8
+            )
+
+            /* MARK: message */
+
+            MessageBox.insert(
+                type: .ok,
+                title: String(format: NSLocalizedString("%d records have been exported", comment: ""), jsonObject.count),
+                lifeTime: .time(3)
+            )
 
         } catch {
             MessageBox.insert(
@@ -89,59 +92,62 @@ final class Features {
             openPanel.allowedContentTypes = [.json]
             openPanel.prompt = NSLocalizedString("Import", comment: "")
 
-            if (openPanel.runModal() == .OK), let fileURL = openPanel.url {
+            guard openPanel.runModal() == .OK else {
+                return
+            }
+            guard let fileURL = openPanel.url else {
+                return
+            }
 
-                let JSONString = try String(
-                    contentsOf: fileURL,
-                    encoding: .utf8
-                )
+            let JSONString = try String(
+                contentsOf: fileURL,
+                encoding: .utf8
+            )
 
-                let JSONObject = try? JSONDecoder().decode(
-                    [ExportImportItem].self,
-                    from: JSONString.data(using: .utf8)!
-                )
+            let JSONObject = try? JSONDecoder().decode(
+                [ExportImportItem].self,
+                from: JSONString.data(using: .utf8)!
+            )
 
-                if (JSONObject == nil) {
-                    MessageBox.insert(
-                        type: .error,
-                        title: NSLocalizedString("Invalid JSON format!", comment: ""),
-                        lifeTime: .time(3)
-                    )
-                    return
-                }
-
-                /* MARK: Import to database */
-
-                var processed: Int = 0
-                var invalidDomains: [String] = []
-                if let items = JSONObject {
-                    for item in items {
-                        if (item.name.domainNameIsValid()) {
-                            let _ = AllowedDomains.delete([item.name])
-                            if (AllowedDomains.insert(name: item.name, isWildcard: item.isGlobal)) {
-                                processed += 1
-                                Logger.customLog("IMPORT ITEM: isWildcard = \(item.isGlobal) | name = \(item.name)")
-                            } else { invalidDomains.append(item.name) }
-                        }     else { invalidDomains.append(item.name) }
-                    }
-                }
-
-                /* MARK: Message */
-
+            guard (JSONObject != nil) else {
                 MessageBox.insert(
-                    type: .ok,
-                    title: String(format: NSLocalizedString("%d records have been imported", comment: ""), processed),
+                    type: .error,
+                    title: NSLocalizedString("Invalid JSON format!", comment: ""),
                     lifeTime: .time(3)
                 )
-                if (!invalidDomains.isEmpty) {
-                    MessageBox.insert(
-                        type: .warning,
-                        title: String(format: NSLocalizedString("Invalid domains were detected:\n%@", comment: ""), invalidDomains.joined(separator: " | ")),
-                        isClosable: true,
-                        lifeTime: .time(10)
-                    )
-                }
+                return
+            }
 
+            /* MARK: Import to database */
+
+            var processedCount: Int = 0
+            var invalidDomains: [String] = []
+            if let items = JSONObject {
+                for item in items {
+                    if (item.name.domainNameIsValid()) {
+                        let _ = AllowedDomains.delete([item.name])
+                        if (AllowedDomains.insert(name: item.name, isWildcard: item.isGlobal)) {
+                            processedCount += 1
+                            Logger.customLog("IMPORT ITEM: isWildcard = \(item.isGlobal) | name = \(item.name)")
+                        } else { invalidDomains.append(item.name) }
+                    }     else { invalidDomains.append(item.name) }
+                }
+            }
+
+            /* MARK: Message */
+
+            MessageBox.insert(
+                type: .ok,
+                title: String(format: NSLocalizedString("%d records have been imported", comment: ""), processedCount),
+                lifeTime: .time(3)
+            )
+            if (!invalidDomains.isEmpty) {
+                MessageBox.insert(
+                    type: .warning,
+                    title: String(format: NSLocalizedString("Invalid domains were detected:\n%@", comment: ""), invalidDomains.joined(separator: " | ")),
+                    isClosable: true,
+                    lifeTime: .time(10)
+                )
             }
 
         } catch {
