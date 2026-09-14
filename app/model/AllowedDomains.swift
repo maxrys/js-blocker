25861c87fa0c35f7ad735970daf94c9eec7c4558
Maxim Rysevets <root@effcore.com>
2026-09-14 13:01:31 +0300

    Migration and Storage improvements: - migration from "WhiteDomains" to "AllowedDomains"; - moved from v.1 "isGlobal" to v.3 "type"; - moved from v.2 "isWildcard" to v.3 "type"; - moved from "ExportImportItemV2" to "ExportImportItemV3"; - entity "AllowedScripts" has been added; - class "ASFetchItem" has been added; - class "ASFetchCollection" has been added; - entity "Migration_001" has been added.

--- app/model/AllowedDomains.swift
+++ app/model/AllowedDomains.swift
@@ -7,9 +7,21 @@ import os
 import AppKit
 import CoreData
 
-typealias AllowedDomains = WhiteDomains
+public class AllowedDomains: NSManagedObject {
 
-extension AllowedDomains {
+    typealias SELF = AllowedDomains
+
+    static let stringName = "AllowedDomains"
+
+    @NSManaged var name: DomainName
+    @NSManaged var nameDecoded: DomainName
+    @NSManaged var type: String
+    @NSManaged var expiresAt: Int64
+    @NSManaged var createdAt: Int64
+
+    convenience init() {
+        self.init(context: Storage.context)
+    }
 
     static func hasDomain(name: DomainName) -> Bool {
         SELF.select(name) != nil
@@ -17,8 +29,8 @@ extension AllowedDomains {
 
     static func matchType(name: DomainName) -> MatchType {
         if let item = SELF.select(name) {
-            if (item.isWildcard != true) { return .exact   (item: item) }
-            if (item.isWildcard == true) { return .wildcard(item: item) }
+            if (item.type == MATCH_TYPE_STRING_EXACT   ) { return .exact   (item: item) }
+            if (item.type == MATCH_TYPE_STRING_WILDCARD) { return .wildcard(item: item) }
         }
         let wildcardDomains = SELF.selectDomainAndTopDomains(name, types: [
             MATCH_TYPE_STRING_WILDCARD,
@@ -38,9 +50,7 @@ extension AllowedDomains {
             request.predicate = {
                 var result:[NSPredicate] = []
                 result.append(NSPredicate(format: "name IN %@", names))
-                if (types == [MATCH_TYPE_STRING_EXACT                            ]) { result.append(NSPredicate(format: "isGlobal <> true")) }
-                if (types == [                         MATCH_TYPE_STRING_WILDCARD]) { result.append(NSPredicate(format: "isGlobal == true")) }
-                if (types == [MATCH_TYPE_STRING_EXACT, MATCH_TYPE_STRING_WILDCARD]) { result.append(NSPredicate(format: "isGlobal <> true OR isGlobal == true")) }
+                result.append(NSPredicate(format: "type IN %@", types))
                 return NSCompoundPredicate(
                     andPredicateWithSubpredicates: result
                 )
@@ -54,12 +64,21 @@ extension AllowedDomains {
         }
     }
 
+    static func selectCount() -> Int {
+        do {
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: SELF.stringName)
+            return try Storage.context.count(for: request)
+        } catch {
+            Logger.customLog("Model \(SELF.stringName).selectCount() error: \(error).")
+            return 0
+        }
+    }
+
     static func select(_ name: DomainName) -> ADFetchItem? {
         do {
-            let orderBy = NSSortDescriptor(key: #keyPath(SELF.createdAt), ascending: false)
             let request = NSFetchRequest<SELF>(entityName: SELF.stringName)
             request.fetchLimit = 1
-            request.sortDescriptors = [orderBy]
+            request.sortDescriptors = [ NSSortDescriptor(key: #keyPath(SELF.createdAt), ascending: false) ]
             request.predicate = NSPredicate(format: "name ==[c] %@", name)
             let result = try Storage.context.fetch(request)
             return result.first.ifNil(defaultValue: nil) { first in
@@ -73,7 +92,6 @@ extension AllowedDomains {
 
     static func selectAll(
         _ filterByName: String? = nil,
-        skipWithExpiration: Bool = false,
         skipExpired: Bool = true,
         orderBy: String = #keyPath(SELF.nameDecoded),
         ascending: Bool = true
@@ -81,14 +99,18 @@ extension AllowedDomains {
         do {
             let request = NSFetchRequest<SELF>(entityName: SELF.stringName)
             request.fetchLimit = Int.max
-            var predicates: [NSPredicate] = []
-            if let filterByName = filterByName { predicates.append(NSPredicate(format: "nameDecoded CONTAINS[cd] %@", filterByName)) }
-            if (skipWithExpiration) { predicates.append(NSPredicate(format: "(expiresAt == 0)")) }
-            if (skipExpired       ) { predicates.append(NSPredicate(format: "(expiresAt == 0) OR (expiresAt > %@)", NSNumber(value: Date.now.int64))) }
-            if (predicates.isEmpty == false) { request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: predicates) }
-            let orderByDefault = NSSortDescriptor(key: orderBy, ascending: ascending)
-            let orderByCreated = NSSortDescriptor(key: #keyPath(SELF.createdAt), ascending: false)
-            request.sortDescriptors = [orderByDefault, orderByCreated]
+            request.predicate = {
+                var result:[NSPredicate] = []
+                if let filterByName { result.append(NSPredicate(format: "nameDecoded CONTAINS[cd] %@", filterByName)) }
+                if (skipExpired)    { result.append(NSPredicate(format: "(expiresAt == 0) OR (expiresAt > %@)", NSNumber(value: Date.now.int64))) }
+                return NSCompoundPredicate(
+                    andPredicateWithSubpredicates: result
+                )
+            }()
+            request.sortDescriptors = [
+                NSSortDescriptor(key: orderBy, ascending: ascending),
+                NSSortDescriptor(key: #keyPath(SELF.createdAt), ascending: false)
+            ]
             return try Storage.context.fetch(request).reduce(into: ADFetchCollection()) { result, modelItem in
                 result.appendUnique(modelItem)
             }
@@ -98,19 +120,22 @@ extension AllowedDomains {
         }
     }
 
-    static func insert(name: DomainName, isWildcard: Bool = false, expiresAt: Int64 = 0) -> ExecuteResult {
+    static func insert(name: DomainName, type: String = MATCH_TYPE_STRING_EXACT, expiresAt: Int64 = 0, createdAt: Int64? = nil, isVersioningDisabled: Bool = false) -> ExecuteResult {
+        let newObject = SELF()
+            newObject.name        = name
+            newObject.nameDecoded = name.decodePunycode()
+            newObject.type        = type
+            newObject.expiresAt   = expiresAt
+            newObject.createdAt   = createdAt ?? Int64(Date.now)
         do {
-            let newObject = SELF()
-                newObject.name        = name
-                newObject.nameDecoded = name.decodePunycode()
-                newObject.isWildcard  = isWildcard
-                newObject.expiresAt   = expiresAt
-                newObject.createdAt   = Int64(Date.now)
             try Storage.context.save()
+            if (!isVersioningDisabled) {
             _ = EntityVersions.versionIncrement(SELF.stringName)
                 EntityVersions.dump()
+            }
             return .success(affected: 1)
         } catch {
+            Storage.context.delete(newObject)
             Logger.customLog("Model \(SELF.stringName).insert() error: \(error).")
             return .failure
         }
@@ -118,20 +143,22 @@ extension AllowedDomains {
 
     static func delete(_ names: [DomainName]) -> ExecuteResult {
         do {
-            let request = NSFetchRequest<SELF>(entityName: SELF.stringName)
-            request.fetchLimit = Int.max
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: SELF.stringName)
             request.predicate = NSPredicate(format: "name IN %@", names)
-            let deleteRequest = NSBatchDeleteRequest(fetchRequest: request as! NSFetchRequest<NSFetchRequestResult>)
-            deleteRequest.resultType = .resultTypeCount
+            let deleteRequest = NSBatchDeleteRequest(fetchRequest: request)
+            deleteRequest.resultType = .resultTypeObjectIDs
             let result = try Storage.context.execute(deleteRequest) as? NSBatchDeleteResult
-            let affected = result?.result as? Int ?? 0
-            try Storage.context.save()
-            if (affected > 0) {
+            let affectedIDs = result?.result as? [NSManagedObjectID] ?? []
+            if (affectedIDs.count > 0) {
+                NSManagedObjectContext.mergeChanges(
+                    fromRemoteContextSave: [NSDeletedObjectsKey: affectedIDs],
+                    into: [Storage.context]
+                )
             _ = EntityVersions.versionIncrement(SELF.stringName)
                 EntityVersions.dump()
             }
             return .success(
-                affected: affected
+                affected: affectedIDs.count
             )
         } catch {
             Logger.customLog("Model \(SELF.stringName).delete() error: \(error).")
@@ -141,20 +168,22 @@ extension AllowedDomains {
 
     static func sanitize() -> ExecuteResult {
         do {
-            let request = NSFetchRequest<SELF>(entityName: SELF.stringName)
-            request.fetchLimit = Int.max
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: SELF.stringName)
             request.predicate = NSPredicate(format: "(expiresAt <> 0) AND (expiresAt < %@)", NSNumber(value: Date.now.int64))
-            let deleteRequest = NSBatchDeleteRequest(fetchRequest: request as! NSFetchRequest<NSFetchRequestResult>)
-            deleteRequest.resultType = .resultTypeCount
+            let deleteRequest = NSBatchDeleteRequest(fetchRequest: request)
+            deleteRequest.resultType = .resultTypeObjectIDs
             let result = try Storage.context.execute(deleteRequest) as? NSBatchDeleteResult
-            let affected = result?.result as? Int ?? 0
-            try Storage.context.save()
-            if (affected > 0) {
+            let affectedIDs = result?.result as? [NSManagedObjectID] ?? []
+            if (affectedIDs.count > 0) {
+                NSManagedObjectContext.mergeChanges(
+                    fromRemoteContextSave: [NSDeletedObjectsKey: affectedIDs],
+                    into: [Storage.context]
+                )
             _ = EntityVersions.versionIncrement(SELF.stringName)
                 EntityVersions.dump()
             }
             return .success(
-                affected: affected
+                affected: affectedIDs.count
             )
         } catch {
             Logger.customLog("Model \(SELF.stringName).sanitize() error: \(error).")
@@ -164,20 +193,17 @@ extension AllowedDomains {
 
     static func dump() {
         #if DEBUG
-            let items = SELF.selectAll(
-                skipWithExpiration: false,
-                skipExpired: false
-            )
+            let items = SELF.selectAll(skipExpired: false)
             if (!items.isEmpty) {
 
                 let rows: [String] = items.reduce(into: []) { result, item in
-                    let formattedName       = item.name
-                    let formattedExpiresAt  = item.expiresAt != 0 ? Date(timeIntervalSince1970: TimeInterval(item.expiresAt)).formatISO8601 : NOT_APPLICABLE
-                    let formattedIsWildcard = item.isWildcard ? "yes" : "no"
+                    let formattedName      = item.name
+                    let formattedExpiresAt = item.expiresAt != 0 ? Date(timeIntervalSince1970: TimeInterval(item.expiresAt)).formatISO8601 : NOT_APPLICABLE
+                    let formattedType      = item.type
                     result.append(">> " +
-                        "\(formattedName      .toWidth(48)) | " +
-                        "\(formattedExpiresAt .toWidth(19)) | " +
-                        "\(formattedIsWildcard.toWidth(11))"
+                        "\(formattedName     .toWidth(48)) | " +
+                        "\(formattedExpiresAt.toWidth(19)) | " +
+                        "\(formattedType     .toWidth(11))"
                     )
                 }
 
@@ -185,9 +211,15 @@ extension AllowedDomains {
 
                 Storage Dump for \"\(SELF.stringName)\":
                 >> ------------------------------------------------------------------------------------
-                >> name                                             |     expires at      | is wildcard
+                >> name                                             |     expires at      |    type
                 >> ====================================================================================
-                \(rows.joined(separator: "\n"))
+                """)
+
+                for line in rows {
+                    Logger.customLog(line)
+                }
+
+                Logger.customLog("""
                 >> ------------------------------------------------------------------------------------
 
                 """)
--- app/model/EntityVersions.swift
+++ app/model/EntityVersions.swift
@@ -66,8 +66,7 @@ public class EntityVersions: NSManagedObject {
         do {
             let request = NSFetchRequest<SELF>(entityName: SELF.stringName)
             request.fetchLimit = Int.max
-            let orderBy = NSSortDescriptor(key: orderBy, ascending: ascending)
-            request.sortDescriptors = [orderBy]
+            request.sortDescriptors = [ NSSortDescriptor(key: orderBy, ascending: ascending) ]
             return try Storage.context.fetch(request)
         } catch {
             Logger.customLog("Model \(SELF.stringName).selectAll() error: \(error).")
@@ -136,7 +135,13 @@ public class EntityVersions: NSManagedObject {
                 >> --------------------------------------------------------------------
                 >> name                                             |     version      
                 >> ====================================================================
-                \(rows.joined(separator: "\n"))
+                """)
+
+                for line in rows {
+                    Logger.customLog(line)
+                }
+
+                Logger.customLog("""
                 >> --------------------------------------------------------------------
 
                 """)
